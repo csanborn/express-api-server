6084f0fc4dacfa7745730d9508b36e957d08428f
Chris Sanborn <granimalcracker@gmail.com>
2021-05-27 13:30:39 -0700

    Clean up index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+// const Joi = require('joi');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
@@ -17,66 +17,6 @@ app.get('/api/weather/:city', async (req, res) => {
     result = await result.json();
     res.status(parseInt(result.cod)).send(result);
 });
-
-// app.post('/api/courses', (req, res) => {
-//     const {error} = validateCourse(req.body);
-
-//     if (error) {
-//         res.status(400).send(error.details[0].message);
-//         return;
-//     }
-
-//     const course = {
-//         id: parseInt(courses[courses.length - 1].id) + 1,
-//         name: req.body.name
-//     }
-
-//     courses.push(course);
-//     res.send(course);
-// });
-
-// app.put('/api/courses/:id', (req, res) => {
-//     const course = courses.find(c => c.id === parseInt(req.params.id));
-
-//     if (!course) {
-//         res.status(404).send(`Course ${req.params.id} not found`)
-//     }
-
-//     const {error} = validateCourse(req.body);
-
-//     if (error) {
-//         res.status(400).send(error.details[0].message);
-//         return;
-//     }
-
-//     course.name = req.body.name;
-//     res.send(course);
-//     // res.send(req.params.id)
-//     // res.send(req.query) get query params
-
-// });
-
-// app.get('/api/courses/:id', (req, res) => {
-//     const course = courses.find(c => c.id === parseInt(req.params.id));
-
-//     if (!course) {
-//         res.status(404).send(`Course ${req.params.id} not found`)
-//     }
-
-//     res.send(course);
-//     // res.send(req.params.id)
-//     // res.send(req.query) get query params
-
-// });
-
-function validateParams(city) {
-    return true
-    const schema = Joi.object({
-        city: Joi.string().min(3).required()
-    })
-
-    return schema.validate(course, schema);
-}
 
 // const port = process.env.PORT || 8080;
 const port = 8080;
